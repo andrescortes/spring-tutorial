6d871c3695189b9931cfb4e58304e13774edac6c
andrescortes <andresabyac@gmail.com>
2020-05-09 18:36:36 -0500

    inicio del proyecto

---
+++ payroll/src/test/java/co/edu/payroll/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.payroll.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
